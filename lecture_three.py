46dfcfdfc8db59c0569905eccd2b1e20209bc4bd
Anmol Srivastava <anmolsri41@gmail.com>
2026-03-20 16:58:40 +0530

    Add files via upload

---
+++ lecture_three.py
@@ -0,0 +1,21 @@
+# marks = [80.8, 90.9, 56.7, 78, 76,]
+# print(type(marks))
+# print(marks)
+# print(marks[4]) 
+
+# student = ("Arju", 88, "gonda")
+# print(student)
+# student = "anmol"
+# print(student)
+
+# marks = (90.1, 56.4, 87.9, 34, 34, 56,)
+# print(marks[1:])
+
+list = [2, 3, 1]
+print(list)
+list.append(4)
+
+
+marks = (90.1, 56.4, 87.9, 34, 34, 56,)
+print(marks[1:])
+
