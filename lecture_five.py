d6f48cd2417b1379ffa15c443327950bdf50980a
Anmol Srivastava <anmolsri41@gmail.com>
2026-06-12 23:26:27 +0530

    new

--- lecture_five.py
+++ lecture_five.py
@@ -38,3 +38,16 @@ i = 1
 while i <= 10:
     print(n * i)
     i += 1 
+
+# Qus4 print the element of the following list using the loop
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+x = 81
+
+i = 0
+while i < len(nums):
+    if(nums[i] == x):
+        print("Found at idx", i)
+    i += 1
+    
+    
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,8 @@
-n = int(input("enter the number"))
-i = 1
-while i <= 10:
-    print(n * i)
-    i += 1 
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+x = 81
+
+i = 0
+while i < len(nums):
+    if(nums[i] == x):
+        print("Found at idx", i)
+    i += 1
