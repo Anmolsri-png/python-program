318847787436384ca74860a645269a56246db2c7
Anmol Srivastava <anmolsri41@gmail.com>
2026-06-12 21:55:11 +0530

    practices qus

--- lecture_five.py
+++ lecture_five.py
@@ -4,3 +4,37 @@ while count <= 5 :
     count += 1
 
 print(count)
+
+i = 1
+while i <= 100:
+    print("hello world" , i)
+    i+=1
+
+i = 5 
+while i >= 1:
+    print(i)
+    i -= 1
+
+print("loop ended")
+
+
+# Qus1 print number from 1 to 100
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+print("loop ended")
+
+# Qus2 print number from 100 to 1
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+
+# Qus3 print the multiplication table of a number n
+n = int(input("enter the number"))
+i = 1
+while i <= 10:
+    print(n * i)
+    i += 1 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,5 @@
-count = 1
-while count <= 5 :
-    print ("hello")
-    count += 1
-
-print(count)
+n = int(input("enter the number"))
+i = 1
+while i <= 10:
+    print(n * i)
+    i += 1 
