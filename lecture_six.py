ed77fb5308b70b55288eb4d651010a4e847730f0
Anmol Srivastava <anmolsri41@gmail.com>
2026-07-12 21:43:57 +0530

    new

--- lecture_six.py
+++ lecture_six.py
@@ -1,11 +1,16 @@
+# def calc_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# calc_sum(5, 10)
+
+# calc_sum(12, 17)
+
+
+# calc_sum(2, 10)
+
 def calc_sum(a, b):
-    sum = a + b
-    print(sum)
-    return sum
-
-calc_sum(5, 10)
-
-calc_sum(12, 17)
-
-
-calc_sum(2, 10)
+    return a + b
+sum = calc_sum(1778, 223)
+print(sum)
