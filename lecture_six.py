499abf26ffca56550ff9290b2871f48982f4e2ca
Anmol Srivastava <anmolsri41@gmail.com>
2026-07-12 23:39:24 +0530

    new

--- lecture_six.py
+++ lecture_six.py
@@ -18,8 +18,18 @@
 # print(sum)
 
 
-def print_hello():
-    print("hello")
+# def print_hello():
+#     print("hello")
 
-output = print_hello()
-print(output)#none value
+# output = print_hello()
+# print(output)#none value
+
+#average of 3 numbers
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+    return avg
+
+calc_avg(98, 97, 97)
