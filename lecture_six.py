f902d723ae279e5dba32e4f8d1abcddb268cf36b
Anmol Srivastava <anmolsri41@gmail.com>
2026-07-13 14:26:59 +0530

    converter

--- lecture_six.py
+++ lecture_six.py
@@ -71,10 +71,17 @@
 
 #WAF to find the factorial of n.(n is the parameter)
 
-def cal_fact(n):
-    fact = 1
-    for i in range(1, n+1):
-        fact *= i
-    print(fact)
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact *= i
+#     print(fact)
 
-cal_fact(6)
+# cal_fact(6)
+
+#WAF to convert USD to INR
+def converter(usd_val):
+    inr_val = usd_val * 93.53
+    print(usd_val, "USD =", inr_val,"INR")
+
+converter(74)
