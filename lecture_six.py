2efe4d1633b5756adb5be648fb0b58ca73242e0e
Anmol Srivastava <anmolsri41@gmail.com>
2026-07-13 14:52:31 +0530

    recursion

--- lecture_six.py
+++ lecture_six.py
@@ -79,9 +79,18 @@
 
 # cal_fact(6)
 
-#WAF to convert USD to INR
-def converter(usd_val):
-    inr_val = usd_val * 93.53
-    print(usd_val, "USD =", inr_val,"INR")
+# WAF to convert USD to INR
+# def converter(usd_val):
+#     inr_val = usd_val * 93.53
+#     print(usd_val, "USD =", inr_val,"INR")
 
-converter(74)
+# converter(74)
+
+# Recursion 
+def show(n):
+    if(n == 0):
+        return
+    print(n)
+    show(n-1)
+     
+show(8)
