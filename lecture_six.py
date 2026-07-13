2febc6334ef0b83b743c3ab4aec91f4d2656aa76
Anmol Srivastava <anmolsri41@gmail.com>
2026-07-13 15:20:57 +0530

    recursion

--- lecture_six.py
+++ lecture_six.py
@@ -87,10 +87,16 @@
 # converter(74)
 
 # Recursion 
-def show(n):
-    if(n == 0):
-        return
-    print(n)
-    show(n-1)
+# def show(n):
+#     if(n == 0):
+#         return
+#     print(n)
+#     show(n-1)
      
-show(8)
+# show(8)
+
+def fact(n):
+    if (n == 1 or n == 0):
+        return 1
+    return fact(n-1) * n
+print(fact(7))
