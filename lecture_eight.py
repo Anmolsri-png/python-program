84cf83f1e031a92399d68b36d2f9e3e881862914
Anmol Srivastava <anmolsri41@gmail.com>
2026-07-17 12:27:19 +0530

    class

--- lecture_eight.py
+++ lecture_eight.py
@@ -1,8 +1,16 @@
-class Student:
-    name = "Vipin"
+# class Student:
+#     name = "Vipin"
 
-s1 = Student()
-print(s1.name)
+# s1 = Student()
+# print(s1.name)
 
-s2 = Student()
-print(s2.name)
+# s2 = Student()
+# print(s2.name)
+
+class Car:
+    color = "blue"
+    brand = "mercedes"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
